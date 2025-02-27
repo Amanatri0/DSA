9a24b838db601577365251aabd0d4e12cb80cf7c
Amanatri0 <atriaman8897@gmail.com>
2025-02-27 12:31:18 +0530

    Duplicate number

---
+++ leetcode/7.RemoveDuplicate.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> nums = { 1, 2, 3, 4, 1, 2, 4 };
+
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if ( nums[i] == nums[j])
+            {
+                cout << "Repeated number" << endl;
+                cout << nums[j] << endl ;
+            }
+
+        }
+
+    }
+
+}
