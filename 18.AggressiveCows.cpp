653263601622b7388f4c3599ebafa248e01230ad
Amanatri0 <atriaman8897@gmail.com>
2025-04-08 16:41:42 +0530

    Aggressive cow done, fruits into basket not completed

---
+++ 18.AggressiveCows.cpp
@@ -0,0 +1,70 @@
+/*
+Assign C cows to N stalls such that min distance between them is largest possible. Return largest minimum distance.
+
+C = 3
+N = 5
+Arr = [1, 2, 8, 4, 9]
+
+
+*/
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std ;
+
+
+bool isPossible(vector<int> &arr, int c, int n, int minAllowedDist) {
+
+    int cows = 1 , lastPos = arr[0];
+
+    for (int i = 1; i < n ; i++){
+        if (arr[i] - lastPos >= minAllowedDist){
+            cows++;
+            lastPos = arr[i];
+        }
+
+        if (cows == c){
+            return true;
+        }
+        
+    }
+    
+    return false;
+
+}
+
+
+int averageDist(vector<int> &arr, int c, int n){
+
+    sort(arr.begin(), arr.end()) ;
+
+    int start = 1 , end = arr[n-1] - arr[0] , ans = -1;
+
+    while(start <= end) {
+        int mid = start + (end - start) / 2 ;
+
+        if (isPossible(arr, c, n, mid)){ // right
+            ans = mid ;
+            start = mid + 1;
+        }else{
+            end = mid - 1 ;
+        }    
+    }
+
+    return ans;
+
+}
+
+
+int main() {
+    vector<int> arr  = {1, 2, 8, 4, 9};
+    int c = 3;
+    int n = 5 ;
+
+    cout << averageDist(arr, c, n) << endl;
+
+    return 0 ;
+
+}
---
+++ 19.FruitsIntoBasket.cpp
@@ -0,0 +1,71 @@
+/*
+You are given two arrays of integers, fruits and baskets, each of length n, where fruits[i] represents the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
+
+From left to right, place the fruits according to these rules:
+
+Each fruit type must be placed in the leftmost available basket with a capacity greater than or equal to the quantity of that fruit type.
+Each basket can hold only one type of fruit.
+If a fruit type cannot be placed in any basket, it remains unplaced.
+Return the number of fruit types that remain unplaced after all possible allocations are made.
+
+ 
+
+Example 1:
+
+Input: fruits = [4,2,5], baskets = [3,5,4]
+
+Output: 1
+
+Explanation:
+
+fruits[0] = 4 is placed in baskets[1] = 5.
+fruits[1] = 2 is placed in baskets[0] = 3.
+fruits[2] = 5 cannot be placed in baskets[2] = 4.
+Since one fruit type remains unplaced, we return 1.
+*/
+
+
+
+//--------------------++++++++++++++++++++++++++++++ NOTE: Not completely correct +++++++++++++++++++++++++++---------------------------
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int fruitsCollected(vector<int> &fruits, vector<int> &baskets) {
+
+    int n = 3 ;
+
+    vector<int> newFruits;
+    vector<int> newBaskets;
+    int remaning =  0 ;
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; i < n; j++){
+            
+            if (fruits[i] <= baskets[j] ){
+                newFruits.push_back(fruits[i]) ;
+                newBaskets.push_back(baskets[j]) ;
+            }
+
+            if (fruits[i] > baskets[j]){
+                remaning = fruits[i] - baskets[j]
+            }
+                
+        }
+        
+    }
+
+    return remaning ;
+    
+}
+
+int main() {
+    vector<int> fruits = {4, 2, 5} ; 
+    vector<int> baskets = {3, 5, 4} ;
+
+    cout << fruitsCollected(fruits, baskets) << endl;
+    return 0;
+}
