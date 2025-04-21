9b70f374f1e78603cc60ccb0a6ecf585f32505ac
Amanatri0 <atriaman8897@gmail.com>
2025-04-21 09:51:02 +0530

    Remove substring

---
+++ 25.RemoveAllStringOccurance.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    string s = "daabcbaabcbc", p = "abc" ;
+    
+    while (s.length() > 0 && s.find(p) < s.length()){
+        s.erase(s.find(p), s.length());
+    }
+    
+
+    return 0;
+
+}
