0936419aa24bf72f2a18461c0ea3480443f51caa
Amanatri0 <atriaman8897@gmail.com>
2025-04-21 16:03:08 +0530

    Reverse String

---
+++ 27.ReverseString.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s = "  hello world  ";
+    int n = s.length();
+  
+    string ans = "";
+  
+    reverse(s.begin(), s.end());
+  
+    for (int i = 0; i < n; i++) {
+      string word = "";
+  
+      while (i < n && s[i] != ' ') {
+        word += s[i];
+        i++;
+      }
+  
+      reverse(word.begin(), word.end());
+  
+      if (word.length() > 0) {
+        ans += " " + word;
+      }
+    }
+  
+    cout << ans.substr(1) << endl;
+    return 0;
+  }
