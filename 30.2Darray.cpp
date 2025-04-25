27359706be0816cf379bd6b691e955a6687432b3
Amanatri0 <atriaman8897@gmail.com>
2025-04-25 16:09:41 +0530

    Diagonal sum of a matrix

--- 30.2Darray.cpp
+++ 30.2Darray.cpp
@@ -52,3 +52,42 @@ int main()
     cout << maxOf << endl;
     return 0;
 }
+
+//------------------------------------------ Diognal sum Of a matrix -------------------------------------------
+
+/*
+Ex:-
+
+    1, 2, 3
+    4, 5, 6
+    7, 8, 9
+
+    sum of = 1 + 5 + 9 and 3 + 5 + 7 where 5 will be common and added only once so the total will be
+    = 25
+*/
+
+int main()
+{
+    int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int row = 3;
+    int col = 3;
+
+    int sum = 0;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i][j];
+            }
+            else if (j == col - 1 - i)
+            {
+                sum += matrix[i][j];
+            }
+        }
+    }
+
+    return sum;
+}
