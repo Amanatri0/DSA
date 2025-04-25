967cdc4e37c94e2d5c9883c089dd295bb032740c
Amanatri0 <atriaman8897@gmail.com>
2025-04-25 16:04:26 +0530

    Maximum sum of a row

--- 30.2Darray.cpp
+++ 30.2Darray.cpp
@@ -22,3 +22,33 @@ int main()
 
     return 0;
 }
+
+// ---------------------------------------------- Maximum row sum ---------------------------------------------------
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+int main()
+{
+    int matrix[3][3] = {{1, 2, 30}, {4, 5, 6}, {7, 8, 9}};
+    int row = 3;
+    int col = 3;
+
+    int maxOf = 0;
+
+    for (int i = 0; i < row; i++)
+    {
+        int sumOf = 0;
+
+        for (int j = 0; j < col; j++)
+        {
+            sumOf += matrix[i][j];
+        }
+
+        maxOf = max(maxOf, sumOf);
+    }
+
+    cout << maxOf << endl;
+    return 0;
+}
