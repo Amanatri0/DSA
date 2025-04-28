1e3c518c1ea342f9a3ff79b20482c4d87d1a2604
Amanatri0 <atriaman8897@gmail.com>
2025-04-28 11:58:55 +0530

    practice previous questions

--- leetcode/1.Two_Sum.cpp
+++ leetcode/1.Two_Sum.cpp
@@ -2,19 +2,23 @@
 #include <vector>
 using namespace std;
 
-vector<int> pairSum(vector<int> nums, int target) {
+vector<int> pairSum(vector<int> nums, int target)
+{
   vector<int> ans;
   int n = nums.size();
 
   // the outer loop will run for once for all the elements in the array
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
 
     // inner loop will add the current element with all the other elements
-    for (int j = i + 1; j < n; j++) {
+    for (int j = i + 1; j < n; j++)
+    {
 
       // "if" statement will check the sum of outer element and inner element is
       // equal to the target
-      if (nums[i] + nums[j] == target) {
+      if (nums[i] + nums[j] == target)
+      {
         ans.push_back(i);
         ans.push_back(j);
         return ans;
@@ -22,11 +26,11 @@ vector<int> pairSum(vector<int> nums, int target) {
     }
   }
 
-
   return ans;
 }
 
-int main() {
+int main()
+{
 
   vector<int> nums = {2, 7, 11, 13, 15};
   int target = 17;
@@ -38,36 +42,80 @@ int main() {
   return 0;
 }
 
-
-
 /*--------------------------------------------------- optimized approach --------------------------------------------*/
 
-
-vector<int> twoSum(vector<int> nums, int target) {
+vector<int> twoSum(vector<int> nums, int target)
+{
   vector<int> ans;
   int n = nums.size();
 
-  int i = 0 , j = n - 1;
+  int i = 0, j = n - 1;
 
-    // sort the nums first
-    // sort(nums.begin(), nums.end());
+  // sort the nums first
+  // sort(nums.begin(), nums.end());
 
-    while( i <  j){
-        int pairSum = nums[i] + nums[j];
-        if (pairSum > target)
-        {
-            j-- ;
-        }else if(pairSum < target ){
-            i++ ;
-        }else{
-            ans.push_back(i);
-            ans.push_back(j);
-            return ans;
-        }
-        
+  while (i < j)
+  {
+    int pairSum = nums[i] + nums[j];
+    if (pairSum > target)
+    {
+      j--;
     }
-
-
+    else if (pairSum < target)
+    {
+      i++;
+    }
+    else
+    {
+      ans.push_back(i);
+      ans.push_back(j);
+      return ans;
+    }
+  }
 
   return ans;
+}
+
+int main()
+{
+  vector<int> nums = {2, 7, 11, 15};
+  int target = 17;
+  int n = nums.size();
+
+  int i = 0, j = n - 1;
+
+  // for (int i = 0; i < nums.size(); i++)
+  // {
+  //   for (int j = i + 1; j < nums.size(); j++)
+  //   {
+  //     if (nums[i] + nums[j] == target)
+  //     {
+  //       cout << "Target found : " << nums[i] << " + " << nums[j] << endl;
+  //     }
+  //   }
+  // }
+
+  vector<int> ans;
+
+  while (i <= j)
+  {
+    int sum = nums[i] + nums[j];
+
+    if (sum > target)
+    {
+      j--;
+    }
+    else if (sum < target)
+    {
+      i++;
+    }
+    else
+    {
+      ans.push_back(nums[i]);
+      ans.push_back(nums[j]);
+      return ans;
+    }
+  }
+
+  return 0;
 }
--- leetcode/2.Major_element.cpp
+++ leetcode/2.Major_element.cpp
@@ -2,25 +2,30 @@
 #include <vector>
 using namespace std;
 
-int main() {
+int main()
+{
   vector<int> nums = {3, 2, 3};
   int n = nums.size();
 
   // the outer loops iterate each of nums value , outer loop will run 6 times
-  for (int val : nums) {
+  for (int val : nums)
+  {
 
     int freq = 0;
 
     // the inner loop will compare the val with each of the element in the nums
     // array eg:- if val = 3 , then it will compare 3 with ele = 3,2,3,
-    for (int ele : nums) {
-      if (ele == val) {
+    for (int ele : nums)
+    {
+      if (ele == val)
+      {
         freq++;
       }
     }
 
     // here if the freq is greater than n/2 then we will return the val
-    if (freq > n / 2) {
+    if (freq > n / 2)
+    {
       cout << "Majority element: " << val << endl;
       return val;
     }
