930e196fd306f3d834ac1f56dec7bcd4e4a4440a
Amanatri0 <atriaman8897@gmail.com>
2025-05-12 19:22:39 +0530

    Recursion and its uses

---
+++ notes&practice/07.Recursion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void printNums(int n)
+{
+
+    if (n == 1)
+    {
+        cout << "1" << endl;
+        return;
+    }
+
+    cout << n << " ";
+
+    printNums(n - 1);
+}
+
+int main()
+{
+    int n = 4;
+    printNums(n);
+
+    return 0;
+}
