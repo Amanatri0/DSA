10b7ac6e3d02e6e5d466c02671fdb041bdf4da96
Amanatri0 <atriaman8897@gmail.com>
2025-05-21 09:47:42 +0530

    Moree's algorithm for finding majority element

--- leetcode/2.Major_element.cpp
+++ leetcode/2.Major_element.cpp
@@ -35,3 +35,61 @@ int main()
   cout << "No majority element found." << endl;
   return 0;
 }
+
+// --------------------------------------------------------- Optimal solution using sorting O(nlogn) ---------------------------------
+
+int main()
+{
+  vector<int> num = {2, 2, 1, 1, 1, 2, 2};
+  int n = num.size();
+  int freq = 1, ans = num[0];
+  sort(num.begin(), num.end());
+
+  for (int i = 1; i < n; i++)
+  {
+    if (num[i] == num[i - 1])
+    {
+      freq++;
+    }
+    else
+    {
+
+      freq = 1;
+      ans = num[i]
+    }
+
+    if (freq > n / 2)
+    {
+      cout << freq << endl;
+    }
+  }
+
+  return 0;
+}
+
+// -------------------------------------------- Most optimal solution using Moree's algorithm  O(n) ------------------------------------------
+
+int main()
+{
+  vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
+  int freq = 0, ans = 0;
+
+  for (int i = 0; i < nums.size(); i++)
+  {
+    if (freq == 0)
+    {
+      ans = nums[i];
+    }
+
+    if (ans == nums[i])
+    {
+      freq++;
+    }
+    else
+    {
+      freq--;
+    }
+  }
+
+  return ans;
+}
