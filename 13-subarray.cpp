1654f8ab04c5ec955412661871a65ad3296ff42c
Amanatri0 <atriaman8897@gmail.com>
2025-06-22 17:34:16 +0530

    GeekForGeek practice

--- 13-subarray.cpp
+++ 13-subarray.cpp
@@ -3,19 +3,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
   int arr[] = {1, 3, 4, 2, 5};
   int size = 5;
 
-    // for the number of lines that the output will run, as the array size is 5 it will run 5 times
-  for (int start = 0; start < size; start++) {
-      
-     // here the start is 0 and the end point is less than the size of the array, so it will run 4 times, the last number in the array will be print by the outer loop.
-    for (int end = start; end < size; end++) {
+  // for the number of lines that the output will run, as the array size is 5 it will run 5 times
+  for (int start = 0; start < size; start++)
+  {
 
-        // this loop will also run 5 times printing all the subarray combinations, here the start is also 0 and the loop will run equal to end times
-      for (int i = start; i <= end; i++) {
+    // here the start is 0 and the end point is less than the size of the array, so it will run 4 times, the last number in the array will be print by the outer loop.
+    for (int end = start; end < size; end++)
+    {
+
+      // this loop will also run 5 times printing all the subarray combinations, here the start is also 0 and the loop will run equal to end times
+      for (int i = start; i <= end; i++)
+      {
         cout << arr[i];
       }
       cout << " ";
@@ -26,40 +30,38 @@ int main() {
   return 0;
 }
 
-
 /*---------------------------------------------- Optimized Approach ------------------------------------------------*/
 
 // Time Complexity O(n2)
 
-
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
   int arr[] = {1, 3, 4, 2, 5};
   int size = 5;
 
-int maxSum = INT8_MAX;
-    // for the number of lines that the output will run, as the array size is 5 it will run 5 times
-  for (int start = 0; start < size; start++) {
+  int maxSum = INT8_MAX;
+  // for the number of lines that the output will run, as the array size is 5 it will run 5 times
+  for (int start = 0; start < size; start++)
+  {
 
-    int currentSum = 0;  
-    
-     // here the start is 0 and the end point is less than the size of the array, so it will run 4 times, the last number in the array will be print by the outer loop.
-    for (int end = start; end < size; end++) {
+    int currentSum = 0;
 
-        currentSum += arr[end];
-        maxSum = max(currentSum, maxSum);
-      
+    // here the start is 0 and the end point is less than the size of the array, so it will run 4 times, the last number in the array will be print by the outer loop.
+    for (int end = start; end < size; end++)
+    {
+
+      currentSum += arr[end];
+      maxSum = max(currentSum, maxSum);
     }
     cout << endl;
   }
 
   return 0;
 }
-
-
 
 /*------------------------------------------ Most Optimized way (Kadane's Algorithm) ------------------------------------*/
 
@@ -69,17 +71,20 @@ int maxSum = INT8_MAX;
 #include <vector>
 using namespace std;
 
-int main() {
+int main()
+{
 
   vector<int> nums = {2, 3, 4, -10, 6, 7, -4};
   // inititalize two variables
   int currentSum = 0, maxSum = 0;
 
-    // foreach loop checks each val in the array nums
-  for (int val : nums) {
-    currentSum += val;                      // currentSum = (currentSum = 0) + (val = 2) ==> 2 initially this will be stored 
-    maxSum = max(currentSum, maxSum);       // here maximum of currentSum and maxSum will be will be stored in maxSum. 
-    if (currentSum < 0) {                   // if the currentSum is less than 0, if the value is (-negative), currentSum will be set ot 0
+  // foreach loop checks each val in the array nums
+  for (int val : nums)
+  {
+    currentSum += val;                // currentSum = (currentSum = 0) + (val = 2) ==> 2 initially this will be stored
+    maxSum = max(currentSum, maxSum); // here maximum of currentSum and maxSum will be stored in maxSum.
+    if (currentSum < 0)
+    { // if the currentSum is less than 0, if the value is (-negative), currentSum will be set ot 0
       currentSum = 0;
     }
     cout << currentSum << endl;
--- leetcode/23.Buy&SellStocks.cpp
+++ leetcode/23.Buy&SellStocks.cpp
@@ -1,3 +1,23 @@
+/*
+Given an array prices[] of length n, representing the prices of the stocks on different days. The task is to find the maximum profit possible by buying and selling the stocks on different days when at most one transaction is allowed. Here one transaction means 1 buy + 1 Sell. If it is not possible to make a profit then return 0.
+
+Note: Stock must be bought before being sold.
+
+Examples:
+
+Input: prices[] = [7, 10, 1, 3, 6, 9, 2]
+Output: 8
+Explanation: You can buy the stock on day 2 at price = 1 and sell it on day 5 at price = 9. Hence, the profit is 8.
+
+Input: prices[] = [7, 6, 4, 3, 1]
+Output: 0
+Explanation: Here the prices are in decreasing order, hence if we buy any day then we cannot sell it at a greater price. Hence, the answer is 0.
+
+Input: prices[] = [1, 3, 6, 9, 11]
+Output: 10
+Explanation: Since the array is sorted in increasing order, we can make maximum profit by buying at price[0] and selling at price[n-1].
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -18,6 +38,41 @@ int main()
     }
 
     cout << maxProf << endl;
+
+    return 0;
+}
+
+// Buy and Sell stocks multiple transactions allowed
+/*
+The cost of stock on each day is given in an array price[]. Each day you may decide to either buy or sell the stock i at price[i], you can even buy and sell the stock on the same day. Find the maximum profit that you can get.
+
+Note: A stock can only be sold if it has been bought previously and multiple stocks cannot be held on any given day.
+
+Input: prices[] = [4, 2, 2, 2, 4]
+Output: 2
+Explanation: Buy the stock on day 3 and sell it on day 4 => 4 – 2 = 2. Maximum Profit = 2.
+
+Input: prices[] = [100, 180, 260, 310, 40, 535, 695]
+Output: 865
+Explanation: Buy the stock on day 0 and sell it on day 3 => 310 – 100 = 210. Buy the stock on day 4 and sell it on day 6 => 695 – 40 = 655. Maximum Profit = 210 + 655 = 865.
+*/
+
+int main()
+{
+    vector<int> arr = {100, 180, 260, 310, 40, 535, 695};
+    int n = arr.size();
+    int maxProfit = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (arr[i] < arr[i + 1])
+        {
+            maxProfit += (arr[i + 1] - arr[i]);
+        }
+    }
+
+    cout << maxProfit << endl;
 
     return 0;
 }
